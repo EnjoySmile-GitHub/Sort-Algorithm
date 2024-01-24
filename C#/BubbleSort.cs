3ba4b88e4016efde09daa847ad49630703efa992
EnjoySmile <enjoysmile99999@gmail.com>
2024-01-24 19:11:21 +0900

    C#でソートの処理を実現
    直接選択法
    バブルソート

---
+++ C#/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class BubbleSort
+    {
+        /// <summary>
+        /// バブルソートによるソート
+        /// </summary>
+        public static void Sort()
+        {
+            int[] data = { 30, 20, 50, 10, 40 };
+
+            for(int i = 0; i < data.Length - 1; i++)
+            {
+                for(int j = data.Length - 1; i < j; j--)
+                {
+                    if (data[j] < data[j - 1])
+                    {
+                        int temp = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = temp;
+                    }
+                }
+            }
+
+            foreach(int num in data)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using C_;
+
+
+// 直接選択法
+DirectSelectionSort.Sort();
+
+// バブルソート
+BubbleSort.Sort();
